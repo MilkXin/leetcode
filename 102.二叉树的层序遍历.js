49098ff348afda562b34a4e9167a4e03608f7661
chenxin <>
2024-07-27 21:45:25 +0800

    refactor: 二叉树的层序遍历

--- 102.二叉树的层序遍历.js
+++ 102.二叉树的层序遍历.js
@@ -19,18 +19,34 @@
  */
 var levelOrder = function (root) {
   // 广度优先遍历，使用队列记录节点和层级，每次取出队列中的节点，将节点值加入到对应层级中
+  //   if (!root) return []
+  //   const res = []
+  //   const q = [[root, 0]]
+  //   while (q.length) {
+  //     const [n, level] = q.shift()
+  //     if (res[level]) {
+  //       res[level].push(n.val)
+  //     } else {
+  //       res[level] = [n.val]
+  //     }
+  //     if (n.left) q.push([n.left, level + 1])
+  //     if (n.right) q.push([n.right, level + 1])
+  //   }
+  //   return res
+
+  // 广度优先遍历，使用队列记录节点，每次取出队列中的节点，将节点值加入到对应层级中
   if (!root) return []
   const res = []
-  const q = [[root, 0]]
+  const q = [root]
   while (q.length) {
-    const [n, level] = q.shift()
-    if (res[level]) {
-      res[level].push(n.val)
-    } else {
-      res[level] = [n.val]
+    let len = q.length
+    res.push([])
+    while (len--) {
+      const n = q.shift()
+      res[res.length - 1].push(n.val)
+      if (n.left) q.push(n.left)
+      if (n.right) q.push(n.right)
     }
-    if (n.left) q.push([n.left, level + 1])
-    if (n.right) q.push([n.right, level + 1])
   }
   return res
 }
